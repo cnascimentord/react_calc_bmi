a37847b78655cdc36ccfdb159c64c9a9954f1b47
Christian Nascimento <christian.nascimento@resultadosdigitais.com.br>
2017-01-03 22:50:14 -0200

    fix eslint

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var serveStatic = require('serve-static');
 
 var app = connect();
 
-app.use(serveStatic(__dirname, {'index': ['index.html']}));
+app.use(serveStatic(__dirname, { index: ['index.html'] }));
 app.listen(3000);
